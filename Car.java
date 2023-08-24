870791714d9bc63e2d2df8fe6b9f2642ad79e054
Yuvashree10 <yuvashrees@stgit.com>
2023-08-24 21:52:37 +0530

    updated an code

---
+++ Car.java
@@ -0,0 +1,29 @@
+public class NewClass2 {
+
+  public static void main (String[] args) {
+    BMW bmw=new BMW();
+   bmw.reverseGear();
+bmw.switchOnHeadlights();
+bmw.topSpeed();
+
+  }
+
+}
+class car{
+  public int no_of_tires = 4;
+  public String bodyType = "Plastic";
+  public void reverseGear(){
+    System.out.println("Reverse Gear is Applied...");
+  }
+   public void switchOnHeadlights(){
+    System.out.println("Headlights turned on...");
+  }
+}
+class BMW extends car{
+  public String modelName = "X3";
+  public void topSpeed()
+  {
+    System.out.println("TopSpeed of BMW is 200 kmph");
+    
+  }
+}
