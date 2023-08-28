9430714179289403298022c111bee2b68489d5ca
Yuvashree Sukumar <yuvashrees@stg.com>
2023-08-28 10:39:54 +0530

    Push changes By person1

--- Car.java
+++ Car.java
@@ -1,32 +1,37 @@
 public class NewClass2 {
 
-  public static void main (String[] args) {
-    BMW bmw=new BMW();
-   bmw.reverseGear();
-bmw.switchOnHeadlights();
-bmw.topSpeed();
+  public static void main(String[] args) {
+    BMW bmw = new BMW();
+    bmw.reverseGear();
+    bmw.switchOnHeadlights();
+    bmw.topSpeed();
 
   }
 
 }
-class car{
+
+class car {
   public int no_of_tires = 4;
   public String bodyType = "Plastic";
-  public void reverseGear(){
-    System.out.println("Reverse Gear is Applied Test Changes");
+
+  public void reverseGear() {
+    System.out.println("Reverse Gear is Applied Merge Arrise By Person1");
   }
-   public void reverseGearConf(){
+
+  public void reverseGearConf() {
     System.out.println("Reverse1 Gear is Applied...");
   }
-   public void switchOnHeadlights(){
+
+  public void switchOnHeadlights() {
     System.out.println("Headlights turned on...");
   }
 }
-class BMW extends car{
+
+class BMW extends car {
   public String modelName = "X3";
-  public void topSpeed()
-  {
+
+  public void topSpeed() {
     System.out.println("TopSpeed of BMW is 900 kmph");
-    
+
   }
 }
